6e913a5eedf6cb15491227d0627b711f0fb8af6f
dielectric <dielectric.army@gmail.com>
2024-04-12 22:02:02 +0800

    del 删除权限

--- src/pages/api/readability.ts
+++ src/pages/api/readability.ts
@@ -3,22 +3,7 @@ import axios from 'axios';
 import {JSDOM} from 'jsdom';
 import {Readability} from '@mozilla/readability';
 
-// 从环境变量中获取预设的 token
-// const presetToken = process.env.PRESET_TOKEN;
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // // 获取 Authorization 头部
-    // const authHeader = req.headers.authorization;
-    // // 检查 Authorization 头部是否存在
-    // if (!authHeader) {
-    //     res.status(401).json({message: 'Authorization header is required.'});
-    //     return;
-    // }
-    // // 检查 Authorization 头部的值是否与预设的 token 匹配
-    // if (authHeader !== `Bearer ${presetToken}`) {
-    //     res.status(401).json({message: 'Invalid token.'});
-    //     return;
-    // }
     // 处理POST请求
     if (req.method === 'POST') {
         let body = req.body
